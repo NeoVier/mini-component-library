2fd11350d91eccb42df719fec7e496b51ac36baa
Henrique Buss <henrique.buss@hotmail.com>
2022-09-04 19:18:09 -0300

    Implement Select element

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,56 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalSelect aria-hidden="true">
+        {displayedValue}
+        <Icon id="chevron-down"
+          strokeWidth={1}
+          size={24}
+        ></Icon>
+      </PresentationalSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  opacity: 0;
+  appearance: none;
+`;
+
+const PresentationalSelect = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+
+  transition: color 150ms ease-in-out;
+
+  padding-inline: 16px;
+  padding-block: 12px;
+  gap: 16px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
